da18f33d97e0c6725ad8e787a3fb0bdade457202
Ricardo Madeira <ricardo.madeira11@hotmail.com>
2021-03-25 20:15:07 +0000

    increase coverage

--- src/test/java/com/todotxt/todotxttouch/util/category_partition/TestStrings.java
+++ src/test/java/com/todotxt/todotxttouch/util/category_partition/TestStrings.java
@@ -1,50 +1,59 @@
 package com.todotxt.todotxttouch.util.category_partition;
 
-import static com.todotxt.todotxttouch.util.Strings.insertPaddedIfNeeded;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.todotxt.todotxttouch.util.Path;
+import com.todotxt.todotxttouch.util.Strings;
 
 public class TestStrings {
 
 	@Test
 	public void testNullStrings() {
-		assertEquals(null,insertPaddedIfNeeded(null, 0, null));
+		assertEquals(null,Strings.insertPaddedIfNeeded(null, 0, null));
 	}
 	
 	@Test(expected=NullPointerException.class)
 	public void testNullPriorString() {
-		assertEquals("string2 ",insertPaddedIfNeeded(null, 0, "string2"));
+		assertEquals("string2 ",Strings.insertPaddedIfNeeded(null, 0, "string2"));
 	}
 	
 	@Test
 	public void testEmptyStrings() {
-		assertEquals("",insertPaddedIfNeeded("", 0, ""));
+		assertEquals("",Strings.insertPaddedIfNeeded("", 0, ""));
 	}
 	
 	@Test
 	public void testEmptyPriorString() {
-		assertEquals("string2 ",insertPaddedIfNeeded("", 0, "string2"));
+		assertEquals("string2 ",Strings.insertPaddedIfNeeded("", 0, "string2"));
 	}
 	
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void testNegativeIndex() {
-		assertEquals("string2 string1",insertPaddedIfNeeded("string1", -4, "string2"));
+		assertEquals("string2 string1",Strings.insertPaddedIfNeeded("string1", -4, "string2"));
 	}
 	
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void testLargerIndex() {
-		assertEquals("string2 string1",insertPaddedIfNeeded("string1", 100, "string2"));
+		assertEquals("string2 string1",Strings.insertPaddedIfNeeded("string1", 100, "string2"));
 	}
 	
 	@Test
 	public void testIndex0() {
-		assertEquals("string2 string1",insertPaddedIfNeeded("string1", 0, "string2"));
+		assertEquals("string2 string1",Strings.insertPaddedIfNeeded("string1", 0, "string2"));
 	}
 	
 	@Test
 	public void testStringsEqual() {
-		assertEquals("string2 ",insertPaddedIfNeeded("string2", 0, "string2"));
+		assertEquals("string2 ",Strings.insertPaddedIfNeeded("string2", 0, "string2"));
+	}
+	
+	@Test
+	public void createString() {
+		Strings s = new Strings();
+		assertTrue(true);
 	}
 	
 }
