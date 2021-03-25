a0712f38ee9263c36b9dd369fe68f4c93bc4f210
André Cruz <andre.cruz@caixamagica.pt>
2021-03-25 19:29:27 +0000

    util.Path with 100% coverage, tests Added

--- src/test/java/com/todotxt/todotxttouch/util/category_partition/TestPath.java
+++ src/test/java/com/todotxt/todotxttouch/util/category_partition/TestPath.java
@@ -1,9 +1,13 @@
 package com.todotxt.todotxttouch.util.category_partition;
 
 import static com.todotxt.todotxttouch.util.Path.fileName;
+import static com.todotxt.todotxttouch.util.Path.parentPath;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.todotxt.todotxttouch.util.Path;
 
 public class TestPath {
 	
@@ -32,6 +36,28 @@ public class TestPath {
 	@Test
 	public void testLargeName() {
 		assertEquals(" qualquercoisa",fileName("nome Pasta / qualquercoisa"));
+	}
+	
+	@Test
+	public void testEndWithBar() {
+		assertEquals("",fileName("/"));
+	}
+	
+	@Test
+	public void createPath() {
+		Path p = new Path();
+		assertTrue(true);
+	}
+	
+	
+	@Test
+	public void testParentPath1() {
+		assertEquals("",parentPath("/"));
+	}
+	
+	@Test
+	public void testParentPath2() {
+		assertEquals("",parentPath("palavra/"));
 	}
 
 }
