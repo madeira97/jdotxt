7de79b13599965c5eb94efbfd7c0dc00b3c7b278
Ricardo Madeira <ricardo.madeira11@hotmail.com>
2021-04-18 17:49:38 +0100

    dataflow tests

---
+++ src/test/java/com/chschmid/jdotxt/dataflow_coverage/TestPath.java
@@ -0,0 +1,63 @@
+package com.chschmid.jdotxt.dataflow_coverage;
+
+import static com.todotxt.todotxttouch.util.Path.fileName;
+import static com.todotxt.todotxttouch.util.Path.parentPath;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.todotxt.todotxttouch.util.Path;
+
+public class TestPath {
+	
+	@Test
+	public void testNullFilename() {
+		assertEquals("",fileName(null)); //pair_id 1
+	}
+	
+	@Test 
+	public void testEmptyFilename() {
+		assertEquals("",fileName("")); //pair_id 1
+	}
+	
+	//ERROR.
+	@Test
+	public void testWindowsFilename() {
+		//assertEquals("nomeIncorreto",fileName("pasta" + "\nomeIncorreto"));
+		assertEquals("pasta\nomeIncorreto",fileName("pasta" + "\nomeIncorreto"));
+	}
+	
+	@Test
+	public void testLinuxFilename() {
+		assertEquals("nomePasta",fileName("pasta/nomePasta"));
+	}
+	
+	@Test
+	public void testLargeName() {
+		assertEquals(" qualquercoisa",fileName("nome Pasta / qualquercoisa"));
+	}
+	
+	@Test
+	public void testEndWithBar() {
+		assertEquals("",fileName("/"));
+	}
+	
+	@Test
+	public void createPath() {
+		Path p = new Path();
+		assertTrue(true);
+	}
+	
+	
+	@Test
+	public void testParentPath1() {
+		assertEquals("",parentPath("/"));
+	}
+	
+	@Test
+	public void testParentPath2() {
+		assertEquals("",parentPath("palavra/"));
+	}
+
+}
---
+++ src/test/java/com/chschmid/jdotxt/dataflow_coverage/TestRelativeDate.java
@@ -0,0 +1,113 @@
+package com.chschmid.jdotxt.dataflow_coverage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.prefs.Preferences;
+
+import org.junit.Test;
+
+import com.chschmid.jdotxt.Jdotxt;
+import com.chschmid.jdotxt.gui.JdotxtGUI;
+import com.todotxt.todotxttouch.util.RelativeDate;
+
+public class TestRelativeDate {
+	
+	private static final long SECOND = 1000; //milliseconds
+	private static final long HOUR = 3600 * SECOND;
+	private static final long DAY = 24 * HOUR;
+	private static final long YEAR = 365 * DAY;
+
+	@Test(expected = NullPointerException.class)
+	public void testDateNull() {
+		Calendar today = new GregorianCalendar();
+		assertEquals("",RelativeDate.getRelativeDate(today,null));
+	}
+	
+	@Test
+	public void testDateYesterday() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-1);
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(JdotxtGUI.lang.getWord("dates_one_day_ago"),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void test3DaysAgo() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-3);
+	    long diff = today.getTimeInMillis() - cal.getTimeInMillis();
+	    long days = diff / DAY;
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(String.format(JdotxtGUI.lang.getWord("dates_days_ago"), days),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void test1MonthAgo() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-31);
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(JdotxtGUI.lang.getWord("dates_one_month_ago"),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void testMonthsAgo() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,-64);
+	    long diff = today.getTimeInMillis() - cal.getTimeInMillis();
+	    long months = diff / (30 * DAY);
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(String.format(JdotxtGUI.lang.getWord("dates_months_ago"), months),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void testFutureDate() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,30);
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(sdf.format(cal.getTime()),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void testAYearAgo() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,30);
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	    JdotxtGUI.loadLookAndFeel(Preferences.userNodeForPackage(Jdotxt.class).get("lang", "English"));
+		assertEquals(sdf.format(cal.getTime()),RelativeDate.getRelativeDate(today,cal));
+	}
+	
+	@Test
+	public void createRelativeDate() {
+		RelativeDate rd = new RelativeDate();
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetRelativeDate_Date() {
+        Date d = new Date();
+        Calendar converted = GregorianCalendar.getInstance();
+        converted.setTime(d);
+		assertEquals(RelativeDate.getRelativeDate(converted),RelativeDate.getRelativeDate(d));
+	}
+	
+	@Test
+	public void testGetRelativeDate_Calendar() {
+		final Calendar today = new GregorianCalendar();
+		Calendar cal = Calendar.getInstance();
+	    cal.add(Calendar.DATE,30);
+		assertEquals(RelativeDate.getRelativeDate(today,cal),RelativeDate.getRelativeDate(cal));
+	}
+
+}
